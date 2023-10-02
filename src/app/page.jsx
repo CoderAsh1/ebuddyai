877981de13d043eafa21d903e4957df4630f9233
coderAsh1 <ashutoshsasmal1@gmail.com>
2023-10-02 18:31:33 +0530

    responsive fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -25,12 +25,12 @@ export default function Home() {
       </header>
       <div className='flex text-center justify-center items-center h-[40vh]'>
         <div className='text-white'>
-          <h1 className='text-6xl mb-3'>Welcome to ExamBuddy AI</h1>
-          <p className='text-slate-900 text-xl '>One stop solution to crack exams with ease.</p>
+          <h1 className='lg:text:6xl md:text-4xl text-4xl mb-3 font-semibold'>Welcome to ExamBuddy AI</h1>
+          <p className='text-slate-700 text-xl font-bold'>One stop solution to crack exams with ease.</p>
         </div>
       </div>
-      <div className='flex gap-10'>
-        <div className="rounded-xl p-6 bg-white phone-1 w-4/12 flex flex-col gap-4">
+      <div className='flex flex-wrap justify-center gap-10 '>
+        <div className="rounded-xl p-6 bg-white phone-1 max-w-sm flex flex-col gap-4">
           <h1 className='font-bold'>Free Plan</h1>
           <p>You will be able to access the companion.</p>
           <div><span className='font-bold text-xl'>&#8377; 0</span>/month</div>
@@ -38,7 +38,7 @@ export default function Home() {
           <Link href="/login" className='mt-auto p-2 px-5 bg-[#331097] hover:bg-[#290c78] transition-colors rounded-md text-white font-bold text-center'>Chat Now
           </Link>
         </div>
-        <div className="rounded-xl p-6 bg-white phone-1 w-4/12 flex flex-col gap-4">
+        <div className="rounded-xl p-6 bg-white phone-1 max-w-sm flex flex-col gap-4">
           <h1 className='font-bold'>Premium Plan</h1>
           <p>You will be able to access the companion 30 days for free from the data of Joining </p>
           <div><span className='font-bold text-xl'>&#8377; 600</span>/month</div>
@@ -46,7 +46,7 @@ export default function Home() {
           <Link href="/" className='mt-auto p-2 px-5 bg-[#331097] hover:bg-[#290c78] transition-colors rounded-md text-white font-bold text-center'>Get Plan
           </Link>
         </div>
-        <div className="rounded-xl p-6 bg-white phone-1 w-4/12 flex flex-col gap-4">
+        <div className="rounded-xl p-6 bg-white phone-1 max-w-sm flex flex-col gap-4">
           <h1 className='font-bold'>Supreme Plan</h1>
           <p>You will be able to access the companion 30 days for free from the data of Joining </p>
 
